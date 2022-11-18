baef60febad3da3fadca05d58ad97ce8a9ecd2bb
JOMADELEMA <jomadelema@gmail.com>
2022-11-17 18:29:54 -0600

    Added the project BlackJack and added the reference for the CardShuffler class library.

---
+++ BlackJack/Program.cs
@@ -0,0 +1,23 @@
+﻿using DeckShuffler;
+using System;
+using System.Collections.Generic;
+
+namespace BlackJack
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = Utils.CreateDeck("BlackJack", 1);
+            Stack<Card> shuffledDeck = Utils.CreateShuffledDeck(deck);
+            foreach(var card in shuffledDeck)
+            {
+                card.CardInfo();
+            }
+            Utils.ShowShuffledDeck(shuffledDeck);
+            Console.ReadKey();
+
+        }
+    }
+}
